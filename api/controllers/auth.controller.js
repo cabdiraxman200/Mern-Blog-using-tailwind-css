2f333c4dfefb4363e8882ee85da4addd01f4636c
AbdirahmanHM <abdirahmandugsiiye51@gmail.com>
2024-08-22 16:42:42 +0300

    add middleware and function to handle erro

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,22 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs';
-export const signup= async(req,res)=>{
+import { errorHandler } from '../utils/error.js';
+export const signup= async(req,res,next)=>{
 const {username,email,password}=req.body;
 if(!username || !email || !password || username==='' || email===''|| password===''){
-    return res.status(400).json({message:"All fields is required"});
+    next(errorHandler(400,"All fields are required"));
 }
 const hashedPassword=bcryptjs.hashSync(password,10)
 const newUser= new User({
-    username,
+    username, 
     email,
     password:hashedPassword,
 })
-await newUser.save();
-res.json("signup sucessfull")
+try{
+    await newUser.save();
+   res.json("signup sucessfull")
+} catch(error){
+   next(error); 
+}
+
 }
--- api/index.js
+++ api/index.js
@@ -8,15 +8,22 @@ const app= express();
 app.use(express.json());
 mongoose.connect(process.env.MONGO)
 .then(()=>{
-    console.log("connecting to mangodb ")
+    console.log("connection  to mangodb ")
 })
 .catch((err)=>{
     console.log(err);
 })
-
-
 app.listen(3000,()=>{
     console.log("sever is running on port 3000")
 });
 app.use("/api/user",userRoutes)
 app.use("/api/auth",authRoutes);
+app.use((err,req,res,next,)=>{
+    const statusCode= err.statusCode || 500;
+    const message= err.message || "internal server error";
+    res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
+})
